d050f9d3869d49014c78d10da290af7f62e68ef3
ananya2118 <ananyannaik3@gmail.com>
2024-07-27 23:22:31 +0530

    event controller updated with routes

--- server/src/routes/eventcontroller.js
+++ server/src/routes/eventcontroller.js
@@ -1,8 +1,44 @@
 import Event from "../models/eventmodel.js"
-
+import express from "express";
 import moment from "moment"
 
-export const createEvents = async (request,response) =>{
+
+const router = express.Router();
+router.get("/", (req, res)=>{
+    res.send("route")
+})
+// export const createEvents = async (request,response) =>{
+//     try{
+//         const event = await new Event(request.body);
+//         event.save();
+//         console.log("event saved successfully");
+    
+//         return response.status(200).json({msg:"Event saved successfully"});
+//         }catch(error)
+//         {
+//             return response.status(500).json({msg:error});
+//         }
+//     };
+
+
+// export const getAllEvents = async (request, response) => {
+
+//     let today = moment().startOf('day').toDate(); 
+    
+//     try {
+
+//             const events = await Event.find({
+//                 date: { $gt: today }
+//             });
+        
+//             return response.status(200).json(events);
+//         } catch (error) {
+//             return response.status(500).json({ msg: error.message });
+//         }
+//     };
+
+
+router.post("/createevents",async(req,res)=>{
     try{
         const event = await new Event(request.body);
         event.save();
@@ -13,11 +49,9 @@ export const createEvents = async (request,response) =>{
         {
             return response.status(500).json({msg:error});
         }
-    };
+})
 
-
-export const getAllEvents = async (request, response) => {
-
+router.get("/getAllEvents",async(req,res)=>{
     let today = moment().startOf('day').toDate(); 
     
     try {
@@ -25,11 +59,11 @@ export const getAllEvents = async (request, response) => {
             const events = await Event.find({
                 date: { $gt: today }
             });
-            
-            // Return the list of events with a 200 status code
+        
             return response.status(200).json(events);
         } catch (error) {
-            // Handle any errors with a 500 status code
             return response.status(500).json({ msg: error.message });
         }
-    };
+})
+
+export {router as eventRouter}
