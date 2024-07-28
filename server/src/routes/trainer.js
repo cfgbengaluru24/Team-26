db881a5a6838c9d159c217f8d9fd1dd3f3d000ee
Agniv Chakraborty <86168859+Agniv25@users.noreply.github.com>
2024-07-28 07:23:07 +0530

    Add files via upload

--- server/src/routes/trainer.js
+++ server/src/routes/trainer.js
@@ -1,34 +1,24 @@
-import express from "express";
-import TrainerModel from "../models/trainerModel.js";
-
+import express from 'express';
+import { TrainerModel } from '../models/trainerModel.js';
 
 const router = express.Router();
 
-// Route to handle the request
-router.get("/filteredEvents/:trainerId", async (req, res) => {
+// Route to get trainer details by ID
+router.get('/:trainerId', async (req, res) => {
   const { trainerId } = req.params;
 
   try {
-    // Find the trainer by ID
-    const trainer = await TrainerModel.findById(trainerId);
+    const trainer = await TrainerModel.findById(trainerId).populate('upcomingEvents').populate('completedEvents');
+    
     if (!trainer) {
-      return res.status(404).json({ message: "Trainer not found" });
+      return res.status(404).json({ message: 'Trainer not found' });
     }
 
-    // Return the result with trainer's data
-    res.json({
-      trainer: {
-        name: trainer.name,
-        email: trainer.email,
-        location: trainer.location,
-        //upcomingEvents: trainer.upcomingEvents,
-      },
-      filteredEvents: filteredEvents,
-    });
+    res.json(trainer);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
-export { router as eventRouter };
+export { router as trainerRouter };
